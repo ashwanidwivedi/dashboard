eaf2655cd60c0e9afcea149f3f498096a2e49e2f
ashwani <ashwani.dwivedi@hotmail.com>
2022-03-25 09:24:15 +0530

    first change

---
+++ WebSocketServer/Middleware/WebSocketServerConnectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+
+namespace WebSocketServer.Middleware
+{
+    public class WebSocketServerConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> _sockets = new ConcurrentDictionary<string, WebSocket>();
+
+        public string AddSocket(WebSocket socket)
+        {
+            //string ConnID = Guid.NewGuid().ToString();
+            string ConnID = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            if (ConnID.Contains('\\'))
+            {
+                ConnID = ConnID.Split('\\')[1];
+            }
+            foreach (var sock in _sockets)
+            {
+                if (sock.Key.ToString() == ConnID)
+                    ConnID = Guid.NewGuid().ToString();
+            }
+            _sockets.TryAdd(ConnID, socket);
+            Console.WriteLine("WebSocketServerConnectionManager-> AddSocket: WebSocket added with ID: " + ConnID);
+            return ConnID;
+        }
+
+        public ConcurrentDictionary<string, WebSocket> GetAllSockets()
+        {
+            return _sockets;
+        }
+    }
+}
---
+++ WebSocketServer/Middleware/WebSocketServerMiddlewareExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebSocketServer.Middleware
+{
+    public static class WebSocketServerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseWebSocketServer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<WebSocketServerMiddleware>();
+        }
+
+        public static IServiceCollection AddWebSocketServerConnectionManager(this IServiceCollection services)
+        {
+            services.AddSingleton<WebSocketServerConnectionManager>();
+            return services;
+        }
+    }
+}
